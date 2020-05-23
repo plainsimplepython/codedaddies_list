338b828c61c667be278d61c1651f5260ceea3b88
Guerrick <rex8nichols@gmail.com>
2020-05-23 13:01:53 -1000

    used cleverprogrammer's solution to getting craigslist post image

--- my_app/views.py
+++ my_app/views.py
@@ -35,7 +35,6 @@ def new_search(request):
     for post in post_listings:
         post_title = post.find(class_= 'result-title').text
         post_url = post.find('a').get('href')
-        post_image_url = CRAIGSLIST_DEFAULT_IMAGE
 
         # get item price if it exists
         if post.find(class_= 'result-price'):
@@ -45,16 +44,11 @@ def new_search(request):
 
 
         if post.find(class_='result-image').get('data-ids'):
-            ''' 
-            If there are images, get the image IDs labeled 'data-ids', formatted as:
-                1:(data-id), 1:(data-id), 1:(data-id),
-            and give the first one from the list
-            '''
-            post_image_ids = post.find(class_='result-image').get('data-ids')
-            post_image = re.findall("1:(.+?),", post_image_ids)
-            if post_image:
-                post_image_url = BASE_IMAGE_URL.format(post_image[0])
-                print(post_image_url)
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = CRAIGSLIST_DEFAULT_IMAGE
 
         final_postings.append((post_title, post_url, post_price, post_image_url))
 
